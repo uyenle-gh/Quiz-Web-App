7df9e87172930d2671dfe5244b5f1df31c50a107
Grace Le <74110113+GraceLe239@users.noreply.github.com>
2022-02-14 00:57:33 -0500

    Update end.js

--- quiz-app/end.js
+++ quiz-app/end.js
@@ -23,6 +23,6 @@ saveHighScore = (e) => {
     highScores.splice(5);
     
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');  
+    window.location.assign('/Quiz-Web-App/quiz-app/');  
 
 }; 
