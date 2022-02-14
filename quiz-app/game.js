f15bb4769dc8b9283d37edeb4181f49a5f6f399c
Grace Le <74110113+GraceLe239@users.noreply.github.com>
2022-02-14 00:55:18 -0500

    Update game.js

--- quiz-app/game.js
+++ quiz-app/game.js
@@ -63,7 +63,7 @@ getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
         //go to the end page
-        return window.location.assign('/end.html');
+        return window.location.assign('/Quiz-Web-App/quiz-app/end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
